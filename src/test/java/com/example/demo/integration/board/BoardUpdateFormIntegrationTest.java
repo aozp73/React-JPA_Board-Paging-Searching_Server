1e4ece81120fd351ad04e5823ccccf0bc1a6475a
aozp73 <aozp73@naver.com>
2024-01-19 17:48:34 +0900

    게시글 수정 페이지 - Integration Test

--- src/test/java/com/example/demo/integration/board/BoardUpdateFormIntegrationTest.java
+++ src/test/java/com/example/demo/integration/board/BoardUpdateFormIntegrationTest.java
@@ -1,12 +1,9 @@
 package com.example.demo.integration.board;
 
-import com.example.demo.module.board.in_dto.BoardUpdate_InDTO;
 import com.example.demo.module.user.User;
 import com.example.demo.module.user.enums.UserRole;
 import com.example.demo.util.DummyEntityHelper;
 import com.example.demo.util.TestSecurityHelper;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -22,18 +18,17 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Transactional
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-public class BoardUpdateIntegrationTest {
+public class BoardUpdateFormIntegrationTest {
 
     @Autowired private MockMvc mockMvc;
     @Autowired private EntityManager em;
-
 
     @BeforeEach
     public void setUp() {
@@ -58,22 +53,15 @@ public class BoardUpdateIntegrationTest {
         em.clear();
     }
 
-    @AfterEach
-    public void tearDown() {
-        SecurityContextHolder.clearContext();
-    }
 
     @Test
-    @DisplayName("게시글 수정 성공")
-    public void update_SuccessTest() throws Exception {
+    @DisplayName("게시글 수정 페이지 응답 성공")
+    public void updateForm_SuccessTest() throws Exception {
         // given
-        BoardUpdate_InDTO boardUpdateInDTO = make_BoardUpdate_InDTO();
-        String content = new ObjectMapper().writeValueAsString(boardUpdateInDTO);
+        Long boardId = 1L;
 
         // when
-        ResultActions resultActions = mockMvc.perform(put("/api/auth/board")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content)
+        ResultActions resultActions = mockMvc.perform(get("/api/auth/board/" + boardId)
                 .accept(MediaType.APPLICATION_JSON));
 
         // then
@@ -82,30 +70,21 @@ public class BoardUpdateIntegrationTest {
                 .andExpect(jsonPath("$.status").value(200))
                 .andExpect(jsonPath("$.msg").value("성공"))
 
-                .andExpect(jsonPath("$.data.boardDetailDTO.boardId").value(1))
-                .andExpect(jsonPath("$.data.boardDetailDTO.title").value("수정 제목1"))
-                .andExpect(jsonPath("$.data.boardDetailDTO.content").value("수정 내용1"))
-                .andExpect(jsonPath("$.data.boardDetailDTO.views").value(10))
-                .andExpect(jsonPath("$.data.boardDetailDTO.createdAt").isNotEmpty())
-                .andExpect(jsonPath("$.data.boardDetailDTO.commentCount").value(0))
-                .andExpect(jsonPath("$.data.boardDetailDTO.user.userId").value(1))
-                .andExpect(jsonPath("$.data.boardDetailDTO.user.username").value("user1"))
-
-                .andExpect(jsonPath("$.data.commentListDTOS").isEmpty())
+                .andExpect(jsonPath("$.data.id").value(1L))
+                .andExpect(jsonPath("$.data.userId").value(1L))
+                .andExpect(jsonPath("$.data.title").value("제목1"))
+                .andExpect(jsonPath("$.data.content").value("내용1"))
                 .andDo(MockMvcResultHandlers.print());
     }
 
     @Test
-    @DisplayName("게시글 수정 실패 - 다른 작성자의 글")
-    public void update_NotMatchWriter_FailTest() throws Exception {
+    @DisplayName("게시글 수정 페이지 응답 실패 - 다른 작성자의 글")
+    public void updateForm_NotMatchWriter_FailTest() throws Exception {
         // given
-        BoardUpdate_InDTO boardUpdateInDTO = BoardUpdate_InDTO.builder().id(2L).title("수정 제목1").content("수정 제목2").build();
-        String content = new ObjectMapper().writeValueAsString(boardUpdateInDTO);
+        Long boardId = 2L;
 
         // when
-        ResultActions resultActions = mockMvc.perform(put("/api/auth/board")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content)
+        ResultActions resultActions = mockMvc.perform(get("/api/auth/board/" + boardId)
                 .accept(MediaType.APPLICATION_JSON));
 
         // then
@@ -117,12 +96,23 @@ public class BoardUpdateIntegrationTest {
                 .andDo(MockMvcResultHandlers.print());
     }
 
+    @Test
+    @DisplayName("게시글 수정 페이지 응답 실패 - 존재하지 않는 게시글 요청")
+    public void updateForm_NotExistBoard_FailTest() throws Exception {
+        // given
+        Long boardId = 3L;
 
-    private BoardUpdate_InDTO make_BoardUpdate_InDTO() {
-        return BoardUpdate_InDTO.builder()
-                .id(1L)
-                .title("수정 제목1")
-                .content("수정 내용1")
-                .build();
+        // when
+        ResultActions resultActions = mockMvc.perform(get("/api/auth/board/" + boardId)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        resultActions
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.msg").value("notFound"))
+                .andExpect(jsonPath("$.data").value("게시물이 존재하지 않습니다."))
+                .andDo(MockMvcResultHandlers.print());
     }
+
 }
